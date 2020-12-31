b3f6fb6816a0de3d4d2042bdaafecade39788123
KaqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2020-12-31 12:24:03 -0300

    Att 2

---
+++ functions/date.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+var data = new Date();
+
+var day = data.getDate();
+
+var monthGet = data.getMonth() + 1;
+var month
+
+if(monthGet = 1){
+    var month = "Janeiro";
+}else if(monthGet = 2){
+    var month = "Fevereiro";
+}else if(monthGet = 3){
+    var month = "Março";
+}else if(monthGet = 4){
+    var month = "Abril";
+}else if(monthGet = 5){
+    var month = "Maio";
+}else if(monthGet = 6){
+    var month = "Junho";
+}else if(monthGet = 7){
+    var month = "Julho";
+}else if(monthGet = 8){
+    var month = "Agosto";
+}else if(monthGet = 9){
+    var month = "Setembro";
+}else if(monthGet = 10){
+    var month = "Outubro";
+}else if(monthGet = 11){
+    var month = "Novembro";
+}else if(monthGet = 12){
+    var month = "Dezembro";
+}
--- screens/main.js
+++ screens/main.js
@@ -1,7 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
-import Espiral from '../assets/espiral2.png'
+import Espiral from '../assets/espiral2.png';
+//import '../functions/date.js';
+
+var data = new Date();
+
+var day = data.getDate();
+
+var monthGet = data.getMonth() + 1;
+var month
+
+if(monthGet == 1){
+    var month = "Janeiro";
+}else if(monthGet == 2){
+    var month = "Fevereiro";
+}else if(monthGet == 3){
+    var month = "Março";
+}else if(monthGet == 4){
+    var month = "Abril";
+}else if(monthGet == 5){
+    var month = "Maio";
+}else if(monthGet == 6){
+    var month = "Junho";
+}else if(monthGet == 7){
+    var month = "Julho";
+}else if(monthGet == 8){
+    var month = "Agosto";
+}else if(monthGet == 9){
+    var month = "Setembro";
+}else if(monthGet == 10){
+    var month = "Outubro";
+}else if(monthGet == 11){
+    var month = "Novembro";
+}else if(monthGet == 12){
+    var month = "Dezembro";
+}
 
 export default function Main() {
     return (
@@ -15,9 +49,20 @@ export default function Main() {
                 style={{height: 600, width: 40, zIndex: 2}}
                 source={Espiral}
             />
-            <View style={{width: '80%'}}>
+            <View style={{width: '80%', flexDirection: 'column'}}>
+              <View style={{height: 50, width: '100%', flexDirection: 'row'}}>
+                <View style={{width: '70%', backgroundColor: 'white', borderWidth: 1, borderColor: 'black', alignItems: 'center'}}>
+                  <TextInput 
+                    style={{height: '100%', width: '100%', padding:10, fontSize: 28}}
+                  />
+                </View>
+                <View style={{width: '30%', backgroundColor: 'white', borderWidth: 1, borderColor: 'black', alignItems: 'center'}}>
+                  <Text style={{fontSize: 23}}>{day}</Text>
+                  <Text style={{fontSize: 15}}>{month}</Text>
+                </View>
+              </View>
               <TextInput 
-                  style={{height: 600, width: '100%', borderWidth: 1, borderColor: 'black', backgroundColor: 'white', textAlignVertical: 'top', padding:10}}
+                  style={{height: 550, width: '100%', borderWidth: 1, borderColor: 'black', backgroundColor: 'white', textAlignVertical: 'top', padding:10}}
                   multiline
               />
             </View>
